c329762836747c71ff58921a58ddc25854df0202
ZinoOhad <zinoohad@gmail.com>
2017-04-24 10:01:07 +0300

    add error file

---
+++ Twitter/Classes/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitter.Classes
+{
+    public class Error
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+}
